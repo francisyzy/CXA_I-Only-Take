82c5436fba1e9b8283267f33b6d973b04946ef6a
Francis Pi <francisyzy.16@ichat.sp.edu.sg>
2017-07-15 00:56:03 +0800

    Initial commit

---
+++ distance.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+import time
+
+while True:
+	GPIO.setmode(GPIO.BCM)
+	
+	TRIG=6
+	ECHO=23
+	
+	print "Distance Measurement In Progress"
+	
+	GPIO.setup(TRIG,GPIO.OUT)
+	GPIO.setup(ECHO,GPIO.IN)
+	
+	GPIO.output(TRIG,GPIO.IN)
+	
+	GPIO.output(TRIG, False)
+	
+	GPIO.output(TRIG, True)
+	
+	GPIO.output(TRIG, True)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, False)
+	
+	while GPIO.input(ECHO)==0:
+	 pulse_start = time.time()
+	
+	while GPIO.input(ECHO)==1:
+	 pulse_end = time.time()
+	
+	pulse_duration = pulse_end - pulse_start
+	
+	distance = pulse_duration * 17150
+	
+	distance = round(distance, 2)
+	
+	print "Distance:", distance, "cm"
+	
+	GPIO.cleanup()
+	if distance > 1000:
+		break
