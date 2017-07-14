a79db6a6b734b1778873166f5d42dae6bbdb23d5
francisyzy <francisyzy>
2017-07-15 07:21:45 +0800

    Latest PY

--- LED.py
+++ LED.py
@@ -12,34 +12,31 @@ config = {
 firebase = pyrebase.initialize_app(config)
 
 db = firebase.database()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(19,GPIO.OUT)
+GPIO.setup(7,GPIO.OUT)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
+GPIO.setup(29,GPIO.OUT)
+def stream_handler(message):
 
-while True:
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setwarnings(False)
-    GPIO.setup(19,GPIO.OUT)
-    GPIO.setup(7,GPIO.OUT)
-    GPIO.setup(11,GPIO.OUT)
-    GPIO.setup(13,GPIO.OUT)
-    GPIO.setup(29,GPIO.OUT)
+    print(message["data"]) # {'title': 'Pyrebase', "body": "etc..."}
+my_stream = db.child("Pi1").child("Pi1").stream(stream_handler)
 
-    def stream_handler(message):
-    	print(message["data"]) # {'title': 'Pyrebase', "body": "etc..."}
+#my_stream.close()
 
-    my_stream = db.child("Pi1").child("Pi1").stream(stream_handler)
+GPIO.output(19,GPIO.HIGH)
+GPIO.output(7,GPIO.HIGH)
+GPIO.output(11,GPIO.HIGH)
+GPIO.output(13,GPIO.HIGH)
+GPIO.output(29,GPIO.HIGH)
+time.sleep(1)
+#print "LED off"
+GPIO.output(19,GPIO.LOW)
+GPIO.output(7,GPIO.LOW)
+GPIO.output(11,GPIO.LOW)
+GPIO.output(13,GPIO.LOW)
+GPIO.output(29,GPIO.LOW)
+time.sleep(1)
 
-    my_stream.close()
-
-    # print "LED on"
-    GPIO.output(19,GPIO.HIGH)
-    GPIO.output(7,GPIO.HIGH)
-    GPIO.output(11,GPIO.HIGH)
-    GPIO.output(13,GPIO.HIGH)
-    GPIO.output(29,GPIO.HIGH)
-    time.sleep(1)
-    # print "LED off"
-    GPIO.output(19,GPIO.LOW)
-    GPIO.output(7,GPIO.LOW)
-    GPIO.output(11,GPIO.LOW)
-    GPIO.output(13,GPIO.LOW)
-    GPIO.output(29,GPIO.LOW)
-    time.sleep(1)
