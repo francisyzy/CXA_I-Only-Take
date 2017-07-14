0a29f1a9d158eda76d9b846ab11778f90fcf04da
Francis Pi <francisyzy.16@ichat.sp.edu.sg>
2017-07-15 06:16:31 +0800

    Transfer LED.py to other IDE

--- LED.py
+++ LED.py
@@ -1,5 +1,17 @@
 import RPi.GPIO as GPIO
 import time
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyD6cr3TxbHq8WPPVc9vaHfwjp--6FlRIdA",
+    "authDomain": "cxapi-c8d4d.firebaseapp.com",
+    "databaseURL": "https://cxapi-c8d4d.firebaseio.com/",
+    "storageBucket": "cxapi-c8d4d.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
 
 while True:
     GPIO.setmode(GPIO.BOARD)
@@ -9,14 +21,22 @@ while True:
     GPIO.setup(11,GPIO.OUT)
     GPIO.setup(13,GPIO.OUT)
     GPIO.setup(29,GPIO.OUT)
-    print "LED on"
+
+    def stream_handler(message):
+    	print(message["data"]) # {'title': 'Pyrebase', "body": "etc..."}
+
+    my_stream = db.child("Pi1").child("Pi1").stream(stream_handler)
+
+    my_stream.close()
+
+    # print "LED on"
     GPIO.output(19,GPIO.HIGH)
     GPIO.output(7,GPIO.HIGH)
     GPIO.output(11,GPIO.HIGH)
     GPIO.output(13,GPIO.HIGH)
     GPIO.output(29,GPIO.HIGH)
     time.sleep(1)
-    print "LED off"
+    # print "LED off"
     GPIO.output(19,GPIO.LOW)
     GPIO.output(7,GPIO.LOW)
     GPIO.output(11,GPIO.LOW)
