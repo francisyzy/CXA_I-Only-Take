7a5d185426d372441d7ece9003c93696a706bf5b
francisyzy <francisyzy>
2017-07-15 08:22:01 +0800

    Restructure the code

--- LEDtwo.py
+++ LEDtwo.py
@@ -1,39 +1,45 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(19,GPIO.OUT)
+GPIO.setup(7,GPIO.OUT)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
+GPIO.setup(29,GPIO.OUT)
+
+def LEDon():
+    GPIO.output(19,GPIO.HIGH)
+    GPIO.output(7,GPIO.HIGH)
+    GPIO.output(11,GPIO.HIGH)
+    GPIO.output(13,GPIO.HIGH)
+    GPIO.output(29,GPIO.HIGH)
+    time.sleep(1)
+    return;
+
+def LEDoff():
+    GPIO.output(19,GPIO.LOW)
+    GPIO.output(7,GPIO.LOW)
+    GPIO.output(11,GPIO.LOW)
+    GPIO.output(13,GPIO.LOW)
+    GPIO.output(29,GPIO.LOW)
+    time.sleep(1)
+    return;
+
+LEDoff
+LEDon
+
 while True:
 
     from firebase import firebase
     firebase = firebase.FirebaseApplication('https://cxapi-c8d4d.firebaseio.com/', None)
     result = firebase.get('/Pi1/Pi1/inBook', None)
-
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setwarnings(False)
-    GPIO.setup(19,GPIO.OUT)
-    GPIO.setup(7,GPIO.OUT)
-    GPIO.setup(11,GPIO.OUT)
-    GPIO.setup(13,GPIO.OUT)
-    GPIO.setup(29,GPIO.OUT)
-
-    def LEDon():
-        GPIO.output(19,GPIO.HIGH)
-        GPIO.output(7,GPIO.HIGH)
-        GPIO.output(11,GPIO.HIGH)
-        GPIO.output(13,GPIO.HIGH)
-        GPIO.output(29,GPIO.HIGH)
-        time.sleep(1)
-        return;
-
-    def LEDoff():
-        GPIO.output(19,GPIO.LOW)
-        GPIO.output(7,GPIO.LOW)
-        GPIO.output(11,GPIO.LOW)
-        GPIO.output(13,GPIO.LOW)
-        GPIO.output(29,GPIO.LOW)
-        time.sleep(1)
-        return;
-
+    print result
+    
     if result:
         LEDon
     else:
         LEDoff
+    sleep(10)
+    print "One loop done"
